8ce672eaf409f31cb859472f723e2d10639bdb2c
Ben <ben@coder.com>
2021-01-13 12:36:15 +0000

    create environment works

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.urlencoded({
+  extended: true
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/actions.js
+++ routes/actions.js
@@ -7,6 +7,8 @@
 var express = require('express');
 var router = express.Router();
 
+const axios = require('axios');
+
 // execute shell commands (in this case, coder-cli)
 const { exec } = require("child_process");
 
@@ -17,30 +19,54 @@ router.get('/', function (req, res, next) {
 
 
 /* POST create workspace. */
-router.post('/create_workspace', function (req, res, next) {
-    // get list of Coder workspaces and images via coder-cli
+router.post('/create_workspace', async (req, res, next) => {
+
+
+    /**
+     * [extremely hacky] attempt to find the Coder URL based off this web app's URL
+     * this will not succeed if this is not hosted on a Coder workspace
+     * 
+     * extended for visibility
+    */
+    let coder_url = req.get("host"); // devurl.coder.tld
+    coder_url = coder_url.split("."); // Array: [devurl, coder, tld] 
+    coder_url.shift() // Array: [coder, tld]
+    coder_url = coder_url.join("."); // coder.tld
+    coder_url = "http://" + coder_url; // http://coder.tld (should forward to HTTPS if enabled, but is backwards-compatable)
+
+    // split image:tag into two seperate vars
+    const [image, tag] = req.body.image.split(":");
+    const name = req.body.name;
+
+    /** attempt to create a Coder workspace
+     * 
+     * this will not currently work for multi-organization deployments
+     * (however, you can potentially fetch the default org by hitting ${coder_url}/api/private/orgs)
+     *  */
+
     // output is JSON, so we put a |||| to split responses (hacky)
-    exec("coder images ls --output json && echo \"|||||\" && coder envs ls --output json", (error, stdout, stderr) => {
-        if (error) {
-            console.error(`error: ${error.message}`);
-            return;
+    exec(`coder envs create ${name} --image ${image} --tag ${tag}`, (error, stdout, stderr) => {
+
+        // bad way to see if it succeeds
+        if (stderr.includes("success")) {
+            exec(`coder envs ls --output json`, (error, stdout, stderr) => {
+
+                let envs = JSON.parse(stdout);
+
+                // look for this environment by the name
+                const this_env = envs.find(env => env.name == name);
+
+                // let's redirect to the Coder screen now :)
+                res.redirect(`${coder_url}/environments?env=${this_env.id}`)
+
+                res.send("Attempting to redirect")
+
+            });
+
+        } else {
+            res.send(stderr + "<br /><hr /><a href=\"/\">Back</a>");
         }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        // no errors, render the index page
 
-        // seperate responses by our "|||||"
-        let responses = stdout.split("|||||");
-
-        // parse the first block of JSON (images)
-        let images = JSON.parse(responses[0]);
-        // parse the second block of JSON (workspaces)
-        let workspaces = JSON.parse(responses[1]);
-
-        // pass on the workspaces list and our environment variables
-        res.send("hello");
     });
 });
 
