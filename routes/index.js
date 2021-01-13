ddc3996feee3672966c5160571c6d4015050c894
Ben <ben@coder.com>
2021-01-13 02:09:44 +0000

    flesh out demo further

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+// execute shell commands (in this case, coder-cli)
+const { exec } = require("child_process");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { env: process.env });
+router.get('/', function (req, res, next) {
+  // get list of Coder workspaces via coder-cli
+  exec("coder envs ls --output json", (error, stdout, stderr) => {
+    if (error) {
+      res.render(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      res.render(`stderr: ${stderr}`);
+      return;
+    }
+    // no errors, render the index page
+    let workspaces = JSON.parse(stdout)
+
+    // pass on the workspaces list and our environment variables
+    res.render('index', { workspaces, env: process.env });
+  });
 });
 
 module.exports = router;
