80c610bf16b9efdccbe430910506facc90018288
Ben <ben@coder.com>
2021-01-13 20:55:23 +0000

    add (non-working) environments demo

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var actionsRouter = require('./routes/actions');
+var onboardingRouter = require('./routes/onboarding');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/actions', actionsRouter);
+app.use('/onboarding', onboardingRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
